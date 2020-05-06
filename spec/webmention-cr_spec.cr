6faaea05ebcfeba92070f3f0a405fdf82a521899
eniehack <eniehack@outlook.jp>
2020-05-07 01:25:20 +0900

    passed webmention.rocks discover test No.3

---
+++ spec/webmention-cr_spec.cr
@@ -0,0 +1,26 @@
+require "./spec_helper"
+require "http/client"
+
+describe WebMention::Discover do
+  it "Webmention.rocks - Discovery Test #1" do
+    url = URI.parse "https://webmention.rocks/test/1"
+    resp = HTTP::Client.get(url)
+    dscv = WebMention::Discover.new(resp, url)
+    dscv.discover.should eq("https://webmention.rocks/test/1/webmention")
+  end
+
+ it "Webmention.rocks - Discovery Test #2" do
+    url = URI.parse "https://webmention.rocks/test/2"
+    resp = HTTP::Client.get(url)
+    dscv = WebMention::Discover.new(resp, url)
+    dscv.discover.should eq("https://webmention.rocks/test/2/webmention")
+  end
+
+
+ it "Webmention.rocks - Discovery Test #3" do
+    url = URI.parse "https://webmention.rocks/test/3"
+    resp = HTTP::Client.get(url)
+    dscv = WebMention::Discover.new(resp, url)
+    dscv.discover.should eq("https://webmention.rocks/test/3/webmention")
+  end
+end
---
+++ src/webmention-cr.cr
@@ -0,0 +1,43 @@
+# TODO: Write documentation for `Webmention::Cr`
+require "http/client"
+require "uri"
+require "xml"
+
+module WebMention
+  VERSION = "0.1.0"
+  class WebMention::Discover
+    def initialize(@resp : HTTP::Client::Response, @target_url : URI)
+      @webmention_endpoint = Set(String).new
+      @endpoint_candidates = Set(URI).new
+    end
+
+    def discover : String
+      if @resp.headers.includes_word?("link", "rel=webmention")
+        strip_header @resp.headers["link"]
+      end
+      strip_html @resp.body
+      make_endpoint
+      return @webmention_endpoint.to_a[0]
+    end
+
+    private def strip_header(str : String)
+      @endpoint_candidates.add URI.parse str.sub(/<((\w|\W)+)>; rel=webmention/) { $1 }
+    end
+
+    private def make_endpoint
+      @endpoint_candidates.each do |element|
+        unless element.absolute?
+          element.scheme = @target_url.scheme
+          element.host = @target_url.host
+        end
+        @webmention_endpoint.add element.to_s
+      end
+    end
+
+    private def strip_html(html : String)
+      XML.parse(html).xpath_nodes("//link[@rel='webmention']/@href").each do |element|
+        @endpoint_candidates.add URI.parse element.text
+      end
+    end
+  end
+end
