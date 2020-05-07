80a296edb668f1859f382949d6986dbc8264ec62
eniehack <eniehack@outlook.jp>
2020-05-07 15:00:42 +0900

    passed discovery test No.4

--- spec/webmention-cr_spec.cr
+++ spec/webmention-cr_spec.cr
@@ -23,4 +23,11 @@ describe WebMention::Discover do
     dscv = WebMention::Discover.new(resp, url)
     dscv.discover.should eq("https://webmention.rocks/test/3/webmention")
   end
+
+  it "Webmention.rocks - Discovery Test #4" do
+    url = URI.parse "https://webmention.rocks/test/4"
+    resp = HTTP::Client.get(url)
+    dscv = WebMention::Discover.new(resp, url)
+    dscv.discover.should eq("https://webmention.rocks/test/4/webmention")
+  end
 end
