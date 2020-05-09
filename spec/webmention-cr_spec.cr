92dfe5c85d162f7b17b436eb7bce3e6c5dfd74dd
eniehack <eniehack@outlook.jp>
2020-05-09 16:52:48 +0900

    Add UserAgent

--- spec/webmention-cr_spec.cr
+++ spec/webmention-cr_spec.cr
@@ -1,48 +1,56 @@
 require "./spec_helper"
 require "http/client"
 
+USERAGENT = HTTP::Headers.new.add("User-Agent", "WebMention.cr test client (https://gitlab.com/eniehack/webmention-cr)")
+
 describe WebMention::Discover do
   it "Webmention.rocks - Discovery Test #1" do
     url = URI.parse "https://webmention.rocks/test/1"
-    resp = HTTP::Client.get(url)
+    resp = HTTP::Client.get(url, USERAGENT)
     dscv = WebMention::Discover.new(resp, url)
     dscv.discover.should eq("https://webmention.rocks/test/1/webmention")
   end
 
- it "Webmention.rocks - Discovery Test #2" do
+  it "Webmention.rocks - Discovery Test #2" do
     url = URI.parse "https://webmention.rocks/test/2"
-    resp = HTTP::Client.get(url)
+    resp = HTTP::Client.get(url, USERAGENT)
     dscv = WebMention::Discover.new(resp, url)
     dscv.discover.should eq("https://webmention.rocks/test/2/webmention")
   end
 
-
- it "Webmention.rocks - Discovery Test #3" do
+  it "Webmention.rocks - Discovery Test #3" do
     url = URI.parse "https://webmention.rocks/test/3"
-    resp = HTTP::Client.get(url)
+    resp = HTTP::Client.get(url, USERAGENT)
     dscv = WebMention::Discover.new(resp, url)
     dscv.discover.should eq("https://webmention.rocks/test/3/webmention")
   end
 
   it "Webmention.rocks - Discovery Test #4" do
     url = URI.parse "https://webmention.rocks/test/4"
-    resp = HTTP::Client.get(url)
+    resp = HTTP::Client.get(url, USERAGENT)
     dscv = WebMention::Discover.new(resp, url)
     dscv.discover.should eq("https://webmention.rocks/test/4/webmention")
   end
 
   it "Webmention.rocks - Discovery Test #5" do
     url = URI.parse "https://webmention.rocks/test/5"
-    resp = HTTP::Client.get(url)
+    resp = HTTP::Client.get(url, USERAGENT)
     dscv = WebMention::Discover.new(resp, url)
     dscv.discover.should eq("https://webmention.rocks/test/5/webmention")
   end
 
   it "Webmention.rocks - Discovery Test #6" do
     url = URI.parse "https://webmention.rocks/test/6"
-    resp = HTTP::Client.get(url)
+    resp = HTTP::Client.get(url, USERAGENT)
     dscv = WebMention::Discover.new(resp, url)
     dscv.discover.should eq("https://webmention.rocks/test/6/webmention")
+  end
+
+  it "Webmention.rocks - Discovery Test #7" do
+    url = URI.parse "https://webmention.rocks/test/7"
+    resp = HTTP::Client.get(url, USERAGENT)
+    dscv = WebMention::Discover.new(resp, url)
+    dscv.discover.should eq("https://webmention.rocks/test/7/webmention")
   end
 
 end
